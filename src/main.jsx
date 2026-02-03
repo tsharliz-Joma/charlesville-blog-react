9609e5d3b066aca30ac81f5c6cec16313a5c0ca0
Charles Joma <almprincecharles@yahoo.com>
2026-02-03 18:27:49 +0900

    Main.jsx :: Update react 17 syntax

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-// Mount the React application. We use React 18's createRoot API
-// which provides improved performance and concurrent rendering features.
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.render(<App />, document.getElementById('root'))
